0831ab30afeee88079acf493ecbd7057aaad8983
ti ka <nguyentrongtin0122@gmail.com>
2021-05-26 12:44:18 +0700

    add ThemDoVatActivity

--- app/src/main/java/com/example/loadhinh/ThemDoVatActivity.kt
+++ app/src/main/java/com/example/loadhinh/ThemDoVatActivity.kt
@@ -3,11 +3,9 @@ package com.example.loadhinh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ThemDoVatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_them_do_vat)
     }
-
-
 }
